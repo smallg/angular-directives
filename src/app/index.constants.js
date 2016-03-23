2b0cb76ecb097172040932e0f98c4829590ef2dd
smallg <gwpxjb@163.com>
2016-03-23 15:09:54 +0800

    init  project

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angularDirectives')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
